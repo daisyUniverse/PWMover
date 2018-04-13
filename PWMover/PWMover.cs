9e49bc3f1cb0ae7a711a991178fc9a732111eec4
Evshaddock <evshaddock@gmail.com>
2018-04-13 05:06:56 -0500

    Added very basic non-functional GUI. And by that I mean I got it to open a window. Don't scoff, it was harder than it sounds...

--- PWMover/PWMover.cs
+++ PWMover/PWMover.cs
@@ -1,22 +1,22 @@
-﻿using System;
+﻿using SharpDX.XInput;
+using System;
+using System.Windows.Forms.Integration;
 using WindowsInput;
 using WindowsInput.Native;
-using SharpDX.XInput;
-using Windows.Foundation;
 
 namespace PWMover
 {
 
     class PWMove
     {
-
+        
         class XInputController
         {
             Controller controller;
             Gamepad gamepad;
             public bool connected = false;
             public int deadband = 2500;
-            public Point leftThumb, rightThumb = new Point(0, 0);
+            public System.Windows.Point leftThumb, rightThumb = new System.Windows.Point(0, 0);
             public float leftTrigger, rightTrigger;
 
             public XInputController()
@@ -43,76 +43,71 @@ namespace PWMover
             }
         }
 
+        [STAThread]
         static void Main(string[] args)
         {
-            InputSimulator sim = new InputSimulator();
-            Console.Title = "PWMove by Elisha Shaddock";
-            Console.BackgroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Clear();
-            Console.WriteLine();
 
+            var wpfwindow = new Window1();
+            InputSimulator sim = new InputSimulator();
+            ElementHost.EnableModelessKeyboardInterop(wpfwindow);
+            wpfwindow.Show();
 
             while (true)
             {
 
                 XInputController controller = new XInputController();
+
                 controller.Update();
 
                 var CLY = controller.leftThumb.Y;
                 var CLX = controller.leftThumb.X;
                 int LY = Convert.ToInt32(CLY);
                 int LX = Convert.ToInt32(CLX);
+
+                Wait(1);
                 int TimeUnit = 100;
+
                 int YP = (int)Math.Round((double)(100 * LY) / TimeUnit);
                 int XP = (int)Math.Round((double)(100 * LX) / TimeUnit);
 
                 if (YP > 1)
                 {
                     sim.Keyboard.KeyDown(VirtualKeyCode.VK_W);
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine(" Y Axis Ontime Percentage:  " + YP);
-                    System.Threading.Thread.Sleep(YP);
-
-                    Console.WriteLine(" Y Axis Offtime Percentage: " + (TimeUnit - YP));
+                    Wait(YP);
                     sim.Keyboard.KeyUp(VirtualKeyCode.VK_W);
-                    System.Threading.Thread.Sleep(TimeUnit - YP);
+                    Wait(TimeUnit - YP);
                 }
                 if (YP < 0)
                 {
                     sim.Keyboard.KeyDown(VirtualKeyCode.VK_S);
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine("-Y Axis Ontime Percentage:  " + (YP * -1));
-                    System.Threading.Thread.Sleep(YP * -1);
-
-                    Console.WriteLine("-Y Axis Offtime Percentage: " + (TimeUnit - (YP * -1)));
+                    Wait(YP * -1);
                     sim.Keyboard.KeyUp(VirtualKeyCode.VK_S);
-                    System.Threading.Thread.Sleep(TimeUnit - (YP * -1));
+                    Wait(TimeUnit - (YP * -1));
                 }
 
                 if (XP > 0)
                 {
                     sim.Keyboard.KeyDown(VirtualKeyCode.VK_D);
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine(" X Axis Ontime Percentage:  " + XP);
-                    System.Threading.Thread.Sleep(XP);
-
-                    Console.WriteLine(" X Axis Offtime Percentage: " + (TimeUnit - XP));
+                    Wait(XP);
                     sim.Keyboard.KeyUp(VirtualKeyCode.VK_D);
-                    System.Threading.Thread.Sleep(TimeUnit - XP);
+                    Wait(TimeUnit - XP);
                 }
                 if (XP < 0)
                 {
                     sim.Keyboard.KeyDown(VirtualKeyCode.VK_A);
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("-X Axis Ontime Percentage:  " + XP * -1);
-                    System.Threading.Thread.Sleep(XP * -1);
-
-                    Console.WriteLine("-X Axis Offtime Percentage: " + (TimeUnit - (XP * -1)));
+                    Wait(XP * -1);
                     sim.Keyboard.KeyUp(VirtualKeyCode.VK_A);
-                    System.Threading.Thread.Sleep(TimeUnit - XP * -1);
+                    Wait(TimeUnit - XP * -1);
                 }
+
             };
+            void Wait(int ms)
+            {
+                DateTime start = DateTime.Now;
+                while ((DateTime.Now - start).TotalMilliseconds < ms)
+                System.Windows.Forms.Application.DoEvents();
+
+            }
         }
     }
 }
